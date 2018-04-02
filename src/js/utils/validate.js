49cce191aa737c0143f143fe895a8e82c5202604
yanghui <wb_yanghui@zhongan.com>
2018-04-02 11:10:02 +0800

    aa

--- src/js/utils/validate.js
+++ src/js/utils/validate.js
@@ -1,47 +1,110 @@
-const MOBILE_REG = /^1\d{10}$/,
-  EMAIL_REG = /^([^@\s]+)@((?:[-a-z0-9]+\.)+[a-z]{2,})$/,
-  CHINESE_REG = /^[\u4e00-\u9fa5]+$/,
-  CHIENG_REG = /^[\u4e00-\u9fa5a-zA-Z]+$/,
-  IDCARD_REG = /(^\d{15}$)|(^\d{18}$)|(^\d{17}(\d|X|x)$)/,
-  MONEY_REG = /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/;
+const INTEGER = /^\d+$/; // 是否0/正整数
+const MONEY = /^([1-9][\d]{0,7}|0)(\.[\d]{1,2})?$/; // 金额
+const MOBILE = /^1\d{10}$/; // 手机号码
+const EMAIL = /^([\w-\.])+@([\w-])+(\.[a-zA-Z]{2,4}){1,2}$/; // 邮箱
+const ORGANIZATION = /^[a-zA-Z0-9]{8}-[a-zA-Z0-9]$/; // 织机构代码
+const UNISOCIALCRECODE = /^[a-zA-Z0-9]{18}$/; // 统一社会信用代码
+const ENGNUM = /^[0-9a-zA-Z]+$/; // 数字和英文
+const USERNAME = /^[^~!@#$%^*+|}{"?/'\\=`]*$/; // 用户姓名（非特殊字符）
+const CHINESE = /^[\u4e00-\u9fa5\s]+$/; // 中文（含空格）
+const IDCARD = /(^\d{15})|(^\d{17}(\d|X|x)$)/; // 身份证
+const URL = /^((https?|ftp|file):\/\/)?([\da-z\.-]+)\.([a-z\.]{2,6})([\/\w \.-]*)*\/?$/; // 网址
 
-class validate {
+const trim = function(val){
+  return String.prototype.trim ? val.trim() : val.replace(/^[\s\uFEFF\xA0]+|[\s\uFEFF\xA0]+$/g, '');
+};
 
-  // 是否数值
+class Validate {
+
+  // 是否为空
+  static isBlank(val) {
+    if (val == undefined || val == '' || val == null) return true;
+    return trim(val + '') == '';
+  }
+
+  // 是否是数值(正负0小数)
   static isNumber(val) {
-    return !isNaN(val);
+    return val != null && !isNaN(val);
   }
 
-  // 是否为手机号码
-  static isMobile(val) {
-    return MOBILE_REG.test(val);
+  static isInteger(val) {
+    return INTEGER.test(val);
   }
 
-  // 是否为邮箱
-  static isEmail(val) {
-    return EMAIL_REG.test(val);
-  }
-
-  // 是否为身份证
-  static isIdCard(val) {
-    return IDCARD_REG.test(val);
-  }
-
-  // 是否为金额
   static isMoney(val) {
-    return MONEY_REG.test(val);
+    return MONEY.test(val);
   }
 
-  // 中英文字符
-  static isChiEng(val) {
-    return CHIENG_REG.test(val);
+  static isMobile(val) {
+    return MOBILE.test(val);
   }
 
-  // 是否为中文
+  static isEmail(val) {
+    return EMAIL.test(val);
+  }
+
+  static isOrganizationCode(val) {
+    return ORGANIZATION.test(val);
+  }
+
+  static isUniSocialCreCode(val) {
+    return UNISOCIALCRECODE.test(val);
+  }
+
+  static isEngNum(val) {
+    return ENGNUM.test(val);
+  }
+
+  static isName(val) {
+    return USERNAME.test(val);
+  }
+
   static isChinese(val) {
-    return CHINESE_REG.test(val);
+    return CHINESE.test(val);
+  }
+
+  static isIdCardSimple(val) {
+    return IDCARD.test(val);
+  }
+
+  static isUrl(val) {
+    return URL.test(val);
+  }
+
+  // 身份证校验
+  static isIdCard(val) {
+    let num = val.toUpperCase();
+    switch (num.length) {
+      case 15:
+        if (!this.isInteger(num)) return false;
+
+        let re = new RegExp(/^(\d{6})(\d{2})(\d{2})(\d{2})(\d{3})$/);
+        let arrSplit = num.match(re);
+
+        //检查生日日期是否正确
+        let dtmBirth = new Date('19' + arrSplit[2] + '/' + arrSplit[3] + '/' + arrSplit[4]);
+
+        let bGoodDay = false;
+        bGoodDay = dtmBirth.getYear() == Number(arrSplit[2]) && dtmBirth.getMonth() + 1 == Number(arrSplit[3]) && dtmBirth.getDate() == Number(arrSplit[4]);
+        return bGoodDay;
+
+      case 18:
+        if (!this.isInteger(num.substr(0, 17))) return false;
+
+        let arrInt = ['7', '9', '10', '5', '8', '4', '2', '1', '6', '3', '7', '9', '10', '5', '8', '4', '2']; //加权因子
+        let code = ['1', '0', 'X', '9', '8', '7', '6', '5', '4', '3', '2', '1']; //效验码
+
+        let checknum = 0;
+        for (let i = 0; i < 17; i++) {
+          checknum += num.substr(i, 1) * arrInt[i];
+        }
+        return code[checknum % 11] == num.substr(17, 1);
+
+      default:
+        return false;
+    }
   }
 
 }
 
-export default validate;
+export default Validate;
