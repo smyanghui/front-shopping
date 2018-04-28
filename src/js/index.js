f7b7596681ed76c54a183883fd466b5469f0fb2f
yanghui <wb_yanghui@zhongan.com>
2018-04-28 18:33:49 +0800

    a

--- src/js/index.js
+++ src/js/index.js
@@ -7,26 +7,49 @@ class Index {
   }
 
   init() {
+
+    // 分类数据
+    this.arrSort = {
+      11: {name: '优惠'},
+      12: {name: '招牌'},
+      13: {name: '吃货最爱'},
+      14: {name: '卡通生日蛋糕'},
+    };
+
+    // 商品数据
     this.arrItem = {
       11: {
-        code: 'xiangcai',
-        name: '湘菜',
+        name: '精品生日水果蛋糕11',
         num: 0,
-        list:[
-          {
-            imgUrl: 'adfasdf',
-            name: 'adf',
-            text: 'asdfasdf',
-          }
-        ]
+        imgUrl: '/src/images/item.png',
+        text: '送蜡烛10支，每个账号限买一个',
+        price: '100.00',
+      },
+      12: {
+        name: '精品生日水果蛋糕12',
+        num: 0,
+        imgUrl: '/src/images/item.png',
+        text: '送蜡烛10支，每个账号限买一个',
+        price: '100.00',
+      },
+      13: {
+        name: '精品生日水果蛋糕13',
+        num: 0,
+        imgUrl: '/src/images/item.png',
+        text: '送蜡烛10支，每个账号限买一个',
+        price: '100.00',
       },
     };
+
+    // 购物车数据
     this.arrCart = {
-      11: {num: 2},
-      12: {num: 1},
+      11: null,
+      12: null,
       13: null,
     };
-    // this.renderItem();
+    this.renderSort();
+    this.renderItem();
+    this.renderCart();
     this.listScroll = new IScroll('#iScrollItem');
   }
 
@@ -34,6 +57,7 @@ class Index {
     const _this = this;
 
     $("#viewCart").click(function(){
+      _this.renderCart();
       $("#pageMask, #cartBox").show();
     });
 
@@ -51,7 +75,7 @@ class Index {
     });
 
     // 修改购物车数量
-    $(".J_cart_choice i").click(function(){
+    $("#cartItemBox").on('click', '.J_cart_choice i', function(){
       const isAdd = $(this).hasClass("icon-add");
       const curId = $(this).closest('li').data('itemid');
       _this.updateCart(curId, isAdd);
@@ -60,31 +84,66 @@ class Index {
   }
 
   // 渲染商品
-  renderItem() {
+  renderSort() {
+    let itemHTML = '';
+    for (let i in this.arrSort) {
+      let item = this.arrSort[i];
+      itemHTML += `<li id="sort_${i}"><p>${item.name}</p></li>`;
+    }
+    $("#sortBox").html(itemHTML);
+  }
 
-    let sortHTML = '';
+  // 渲染商品
+  renderItem() {
     let itemHTML = '';
     for (let i in this.arrItem) {
-      let items = this.arrItem[i];
-      sortHTML += `<li id="${items.code}"><p>${items.name}</p></li>`;
-
-      let listHTML = '<ul>';
-      for (let j in items.list) {
-        let list = items.list[j];
-        listHTML += `<li>${list.name}</li>`;
-      }
-      listHTML += '</ul>';
-      itemHTML += listHTML;
+      let item = this.arrItem[i];
+      itemHTML += `<li id="item_${i}" data-itemid="${i}">
+        <p class="image_box"><img src="${item.imgUrl}" /></p>
+        <div class="text_box">
+          <p class="item_tit">${item.name}</p>
+          <div class="item_remark">
+            <p>${item.text}</p>
+          </div>
+          <div class="price_box">
+            <p class="item_price">￥${item.price}</p>
+            <p class="item_choice J_item_choice">
+              <i class="iconfont icon-minus"></i>
+              <strong>${item.num}</strong>
+              <i class="iconfont icon-add"></i>
+            </p>
+          </div>
+        </div>
+      </li>`;
     }
+    $("#itemBox").html(itemHTML);
+  }
 
-    console.log(sortHTML, itemHTML);
+  // 渲染购物车
+  renderCart() {
+    let itemHTML = '';
+    for (let i in this.arrCart) {
+      let item = this.arrCart[i];
+      if (!item) continue;
+      itemHTML += `<li id="cart_${i}" data-itemid="${i}">
+        <div class="cart_item">
+            <p class="cart_item_tit">${item.name}</p>
+            <p class="cart_item_sm">${item.price}</p>
+        </div>
+        <div class="cart_choice J_cart_choice">
+          <i class="iconfont icon-minus"></i>
+          <strong>${item.num}</strong>
+          <i class="iconfont icon-add"></i>
+        </div>
+      </li>`;
+    }
+    $("#cartItemBox").html(itemHTML);
   }
 
   // 移入/移出购物车
   changeCart(cid, isadd) {
     let curNum = parseInt(this.arrItem[cid].num);
     let resNum = isadd ? ++curNum : --curNum;
-
     this.arrItem[cid].num = resNum;
     // 更新加入购物车
     let arrCart = this.arrCart;
@@ -97,26 +156,24 @@ class Index {
     } else {
       if (resNum > 0) arrCart[cid] = this.arrItem[cid];
     }
-
+    this.saveSession();
     $("#item_"+ cid).find("strong").text(resNum);
-
-//   window.sessionStorage['aa'] = aa;
-//   window.sessionStorage['cartArr'] = cartArr;
   }
 
   // 修改购物车
   updateCart(cid, isadd) {
     let curNum = parseInt(this.arrCart[cid].num);
     let resNum = isadd ? ++curNum : --curNum;
+    this.arrCart[cid].num = this.arrItem[cid].num = resNum;
+    this.saveSession();
+    $("#cart_"+ cid +", #item_"+ cid).find("strong").text(resNum);
+  }
 
-    this.arrCart[cid].num = resNum;
-    //= this.arrItem[cid].num
-
-    $("#cart_"+ cid).find("strong").text(resNum);
-
-    //   
-//   arra[curId].num = resNum;
-//   $("#id").find('b').text();
+  // 暂存数据
+  saveSession() {
+    sessionStorage.arrItem = this.arrItem;
+    sessionStorage.arrCart = this.arrCart;
+    // $.isEmptyObject(aa); 判断是否为空对象
   }
 
 }
